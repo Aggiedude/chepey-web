e06e6004e03d887b9b75306064938b707beb7c2c
Andrew Chepey <achepey@gmail.com>
2015-07-30 22:48:34 -0500

    staged the rest of files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,22 @@
-/**
- * Created by achepey on 7/30/15.
- */
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    jshint = require('gulp-jshint');
+
+gulp.task('compile-scss', function() {
+    gulp.src('./css/site.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./scss/**/*.scss', ['compile-scss']);
+    gulp.watch('./css/site.scss', ['compile-scss']);
+})
+
+gulp.task('default', ['compile-scss', 'lint', 'watch']);
--- js/app.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by achepey on 7/30/15.
- */
